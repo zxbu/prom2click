6409ba1ae7adb56da73e4c023d302284cc9fb955
zhouxin <zhouxin@yijiupi.com>
2020-12-28 15:50:55 +0800

    性能优化

--- writer.go
+++ writer.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -53,7 +54,8 @@ func NewP2CWriter(conf *config, reqs chan *p2cRequest) (*p2cWriter, error) {
 
 func newKafka(conf *config) (sarama.AsyncProducer, error) {
 	kafkaConfig := sarama.NewConfig()
-	kafkaConfig.Producer.RequiredAcks = sarama.WaitForAll
+	kafkaConfig.Producer.RequiredAcks = sarama.WaitForLocal
+	kafkaConfig.Producer.Flush.Frequency = 500 * time.Millisecond
 	kafkaConfig.Producer.Partitioner = sarama.NewRandomPartitioner
 	kafkaConfig.Producer.Return.Successes = true
 	kafkaConfig.Producer.Return.Errors = true
@@ -68,6 +70,49 @@ type p2cRequestJson struct {
 	Tags []string    `json:"tags"`
 	Val  interface{} `json:"val"`
 	Ts   int64       `json:"ts"`
+}
+
+func send(w *p2cWriter, producer sarama.AsyncProducer) {
+	w.wg.Add(1)
+	fmt.Println("Writer starting..")
+	ok := true
+	for ok {
+		// get next batch of requests
+		var req *p2cRequest
+		req, ok = <-w.requests
+		if !ok {
+			fmt.Printf("receive chan error, break.\n")
+			break
+		}
+		reqJson := new(p2cRequestJson)
+		reqJson.Name = req.name
+		reqJson.Tags = req.tags
+		if math.IsNaN(req.val) {
+			reqJson.Val = "NaN"
+		} else if math.IsInf(req.val, 1) {
+			reqJson.Val = "Inf"
+		} else if math.IsInf(req.val, -1) {
+			reqJson.Val = "-Inf"
+		} else {
+			reqJson.Val = req.val
+		}
+		reqJson.Ts = req.ts.Unix()
+		jsonMarshal, jsonErr := json.Marshal(&reqJson)
+		if jsonErr != nil {
+			fmt.Printf("Error Marshal json, name: %s, val: %f,  error: %s\n", reqJson.Name, req.val, jsonErr.Error())
+			continue
+		}
+		//fmt.Printf("json: %s\n", jsonMarshal)
+		msg := sarama.ProducerMessage{
+			Topic: w.conf.KafkaTopic,
+			Value: sarama.ByteEncoder(jsonMarshal),
+		}
+
+		producer.Input() <- &msg
+	}
+	defer producer.AsyncClose()
+	fmt.Println("Writer stopped..")
+	w.wg.Done()
 }
 
 func (w *p2cWriter) Start() {
@@ -86,48 +131,9 @@ func (w *p2cWriter) Start() {
 		}
 	}()
 
-	go func() {
-		w.wg.Add(1)
-		fmt.Println("Writer starting..")
-		ok := true
-		for ok {
-			// get next batch of requests
-			var req *p2cRequest
-			req, ok = <-w.requests
-			if !ok {
-				fmt.Printf("receive chan error, break.\n")
-				break
-			}
-			reqJson := new(p2cRequestJson)
-			reqJson.Name = req.name
-			reqJson.Tags = req.tags
-			if math.IsNaN(req.val) {
-				reqJson.Val = "NaN"
-			} else if math.IsInf(req.val, 1) {
-				reqJson.Val = "Inf"
-			} else if math.IsInf(req.val, -1) {
-				reqJson.Val = "-Inf"
-			} else {
-				reqJson.Val = req.val
-			}
-			reqJson.Ts = req.ts.Unix()
-			jsonMarshal, jsonErr := json.Marshal(&reqJson)
-			if jsonErr != nil {
-				fmt.Printf("Error Marshal json, name: %s, val: %f,  error: %s\n", reqJson.Name, req.val, jsonErr.Error())
-				continue
-			}
-			//fmt.Printf("json: %s\n", jsonMarshal)
-			msg := sarama.ProducerMessage{
-				Topic: w.conf.KafkaTopic,
-				Value: sarama.ByteEncoder(jsonMarshal),
-			}
-
-			producer.Input() <- &msg
-		}
-		defer producer.AsyncClose()
-		fmt.Println("Writer stopped..")
-		w.wg.Done()
-	}()
+	for i := 0; i < 5; i++ {
+		go send(w, producer)
+	}
 }
 
 func (w *p2cWriter) Wait() {
